7f7f7c65897caf25ec79666d80222b763833351d
Jacob Ho <clh161@gmail.com>
2022-12-07 02:22:08 +0000

    Test Sentry

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,8 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
+	implementation("io.sentry:sentry-spring-boot-starter:6.9.2")
+	implementation("io.sentry:sentry-logback:6.9.2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- src/main/kotlin/com/sentrytest/demo/TestController.kt
+++ src/main/kotlin/com/sentrytest/demo/TestController.kt
@@ -1,13 +1,22 @@
 package com.sentrytest.demo
 
+import io.sentry.Sentry
+import io.sentry.spring.tracing.SentrySpan
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
+
 @RestController
 class TestController {
     @RequestMapping(value = ["test"], method = [RequestMethod.GET])
+    @SentrySpan
     fun test(): String {
+        try {
+            throw Exception("This is a test.")
+        } catch (e: Exception) {
+            Sentry.captureException(e)
+        }
         return "OK"
     }
 }
