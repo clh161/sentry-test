81c119d38ee54ea07fd78c48026b12ddff0fe145
Jacob Ho <clh161@gmail.com>
2022-12-07 02:17:10 +0000

    Add test controller

---
+++ src/main/kotlin/com/sentrytest/demo/TestController.kt
@@ -0,0 +1,13 @@
+package com.sentrytest.demo
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController {
+    @RequestMapping(value = ["test"], method = [RequestMethod.GET])
+    fun test(): String {
+        return "OK"
+    }
+}
